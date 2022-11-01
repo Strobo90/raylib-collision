230f4e9de2333337b2754286d333cf2fda87d65e
Strobo90 <113635128+Strobo90@users.noreply.github.com>
2022-11-01 13:35:50 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace HelloWorld
+{
+    static class Program
+    {
+        public static void Main()
+        {
+
+            var ScreenHeight = 480;
+            var ScreenWidth = 800;
+            var RectangleSize = 50;
+
+            var PlayerRectangle = new Rectangle(ScreenWidth - (RectangleSize * 2), ScreenHeight - (RectangleSize * 2), RectangleSize, RectangleSize);
+            var TargetRectangle = new Rectangle(100, 100, RectangleSize, RectangleSize);
+            var MovementSpeed = 4;
+
+            Raylib.InitWindow(ScreenWidth, ScreenHeight, "Ball");
+            Raylib.SetTargetFPS(60);
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.DrawText("Move the red square to the blue square with the arrow keys!", 12, 12, 20, Color.BLACK);
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)) {
+                    PlayerRectangle.x += MovementSpeed;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT)) {
+                    PlayerRectangle.x -= MovementSpeed;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_UP)) {
+                    PlayerRectangle.y -= MovementSpeed;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN)) {
+                    PlayerRectangle.y += MovementSpeed;
+                }
+
+                Raylib.DrawRectangleRec(TargetRectangle, Color.BLUE);
+                Raylib.DrawRectangleRec(PlayerRectangle, Color.RED);
+
+                if (Raylib.CheckCollisionRecs(PlayerRectangle, TargetRectangle)) {
+                    Raylib.DrawText("You did it!!!!", 12, 34, 20, Color.BLACK);
+                }
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
